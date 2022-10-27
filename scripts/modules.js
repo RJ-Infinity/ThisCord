cb68d6ef3da4bbe69100e60fee2c388b87e25efb
RJ_Infinity <rjinfinity2004@gmail.com>
2022-10-27 23:19:52 +0100

    added emoji modules

--- scripts/modules.js
+++ scripts/modules.js
@@ -1,9 +1,26 @@
+function CheckModules(){
+	if (ThisCord.DiscordModules.length < 1){
+		window.ThisCord.updateModules();
+	}
+}
+
 function getToken(){
-	window.ThisCord.updateModules();
+	CheckModules();
 	return ThisCord.DiscordModules.find(
 		m => m?.exports?.default?.getToken !== void 0
 	).exports.default.getToken()
 }
-exports({getToken})
 
-// Promise
+function ToCodePoint(emojiChar,seperator){
+	CheckModules();
+	return ThisCord.DiscordModules.find(m=>m.id==742358).exports.Z.convert.toCodePoint(emojiChar,seperator);
+}
+function getEmojiURL(emojiCodePoint){
+	CheckModules();
+	return ThisCord.DiscordModules.find(m=>m.id==553895).exports("./"+emojiCodePoint+".svg")
+}
+function testEmoji(e){
+	CheckModules();
+	return ThisCord.DiscordModules.find(m=>m.id==742358).exports.Z.test(e);
+}
+exports({getToken,ToCodePoint,getEmojiURL,testEmoji})

22425d61e7d1daf8115136311c10a4738e95258f
RJ_Infinity <rjinfinity2004@gmail.com>
2022-10-28 23:17:13 +0100

    now does timestamps so can render any discord message

--- scripts/showCrossServerReplys.js
+++ scripts/showCrossServerReplys.js
@@ -41,6 +41,67 @@ function resolveRoleMentions(el, serverID){
 		})
 	);
 }
+function resolveRelativeTime(el){
+	Array.from(el.querySelectorAll("[ThisCordRelativeTime]")).forEach(el=>{
+		setTime(el)
+	});
+}
+function formatTime(past,unit,amount){
+	if (past){
+		return (amount == 1?"a":amount)+" "+unit+(amount == 1?"":"s")+" ago";
+	}
+	return "in "+(amount == 1?"a":amount)+" "+unit+(amount == 1?"":"s");
+}
+function setTime(el){
+	if (!document.body.contains(el)){
+		return; // the element is no longer in the dom so stop setting the time
+	}
+	var diff = (new Date).getTime()/1000 - (el.getAttribute("timestamp"))
+	past = diff > 0;
+	diff = Math.abs(diff);
+	if (diff < 1){
+		el.innerText = "now";
+		setTimeout(setTime,500,el);
+	}
+	if (diff < 60){
+		el.innerText = formatTime(past,"second",Math.round(diff));
+		setTimeout(setTime,(diff - (Math.round(diff)-1))*1000,el);
+		return;
+	}
+	if (diff < 3600){
+		el.innerText = formatTime(past,"minute",Math.round(diff/60));
+		setTimeout(setTime,(diff - (Math.round(diff/60)-1)*60)*1000,el);
+		return;
+	}
+	if (diff < 86400){
+		el.innerText = formatTime(past,"hour",Math.round(diff/3600));
+		setTimeout(setTime,(diff - (Math.round(diff/3600)-1)*3600)*1000,el);
+		return;
+	}
+	// although from this point on the numbers might deviate slightly from what discord have i think
+	// that mine is slightly more accurate as time from this point on is more relative than absolute
+	// so you cannot say that somthing is a month ago only that it happened in the last month which
+	// could be only seconds ago because a month is different lengths of time depending on how where
+	// in the year it is positioned
+	var now = new Date;
+	var date = new Date(el.getAttribute("timestamp")*1000);
+	if (
+		date.getFullYear() == now.getFullYear() &&
+		date.getMonth() == now.getMonth()
+	){
+		el.innerText = formatTime(past,"day",Math.round(diff/86400));
+		setTimeout(setTime,(diff - (Math.round(diff/86400)-1)*86400)*1000,el);
+		return;
+	}
+	if (date.getFullYear() == now.getFullYear()){
+		el.innerText = formatTime(past,"month",Math.abs(date.getMonth() - now.getMonth()));
+		setTimeout(setTime,86400000,el);
+		return;
+	}
+	el.innerText = formatTime(past,"year",Math.abs(date.getFullYear() - now.getFullYear()));
+	setTimeout(setTime,86400000,el);
+	return;
+}
 
 function ParseContent(content){
 	content = Sanitise(content)
@@ -133,6 +194,69 @@ function ParseContent(content){
 			)
 		}
 	);
+	content.match(/&lt;t:[0-9]+(:[tTdDfFR])?&gt;/g)?.forEach(m=>{
+		var n = m.substring(6,m.length-4);
+		n = n + (n.indexOf(":") > 0?"":":f");
+		var format = n[n.length-1];
+		n = n.substring(0,n.length-2);
+		var time = new Date(n * 1000);
+		var replace = "";
+		switch (format){
+			case "t":{
+				replace = "<span class=\"timestamp-6-ptG3\">"+
+				("00" + time.getHours()).slice(-2)+
+				":"+("00" + time.getMinutes()).slice(-2)
+				"</span>";
+			}break;
+			case "T":{
+				replace = "<span class=\"timestamp-6-ptG3\">"+
+				("00" + time.getHours()).slice(-2)+
+				":"+("00" + time.getMinutes()).slice(-2)+
+				":"+("00" + time.getSeconds()).slice(-2)
+				"</span>";
+			}break;
+			case "d":{
+				replace = "<span class=\"timestamp-6-ptG3\">"+
+				("00" + time.getDate()).slice(-2)+
+				"/"+("00" + (time.getMonth()+1)).slice(-2)+
+				"/"+("0000" + (time.getFullYear()+1)).slice(-4)
+				"</span>";
+			}break;
+			case "D":{
+				replace = "<span class=\"timestamp-6-ptG3\">"+
+				("00" + time.getDate()).slice(-2)+
+				" "+time.toLocaleString('default', { month: 'long' })+
+				" "+("0000" + (time.getFullYear()+1)).slice(-4)
+				"</span>";
+			}break;
+			case "f":{
+				replace = "<span class=\"timestamp-6-ptG3\">"+
+				("00" + time.getDate()).slice(-2)+
+				" "+time.toLocaleString('default', { month: 'long' })+
+				" "+("0000" + (time.getFullYear()+1)).slice(-4)+
+				" "+("00" + time.getHours()).slice(-2)+
+				":"+("00" + time.getMinutes()).slice(-2)
+				"</span>";
+			}break;
+			case "F":{
+				replace = "<span class=\"timestamp-6-ptG3\">"+
+				time.toLocaleDateString("default", { weekday: 'long' })+
+				", "+("00" + time.getDate()).slice(-2)+
+				" "+time.toLocaleString('default', { month: 'long' })+
+				" "+("0000" + (time.getFullYear()+1)).slice(-4)+
+				" "+("00" + time.getHours()).slice(-2)+
+				":"+("00" + time.getMinutes()).slice(-2)+
+				"</span>";
+			}break;
+			case "R":{
+				replace = `<span class=\"timestamp-6-ptG3\" timestamp=\"${n}\" ThisCordRelativeTime></span>`;
+			}break;
+			default:{
+				replace=m;
+			}break;
+		}
+		content = content.replaceAll(m,replace);
+	});
 	return content;
 }
 function Sanitise(content){
@@ -203,6 +327,7 @@ function messageLink(el,paths){
 		resolveMentions(el);
 		resolveChannelMentions(el,paths[0]);
 		resolveRoleMentions(el,paths[0]);
+		resolveRelativeTime(el);
 	})
 }
 function channelLink(el,paths){

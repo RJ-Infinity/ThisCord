76f3b929988ee033a851798803580748808c7761
RJ_Infinity <rjinfinity2004@gmail.com>
2022-10-29 12:20:31 +0100

    now messages show if they have an embed or other content

--- scripts/showCrossServerReplys.js
+++ scripts/showCrossServerReplys.js
@@ -57,6 +57,20 @@ function messageLink(el,paths){
 			<img class="ThisCord-embed-icon" src="https://cdn.discordapp.com/avatars/${json["author"]["id"]}/${json["author"]["avatar"]}">
 			<b>${messageRenderer.Sanitise(json["author"]["username"])}</b>
 			<p>${messageRenderer.ParseContent(json["content"])}</p>
+			${(
+				json?.attachments?.length > 0 ||
+				json?.embeds?.length > 0 ||
+				json?.components?.length > 0 ||
+				json?.sticker_items?.length > 0 ||
+				json?.stickers?.length > 0
+			)?`
+			<svg class="repliedTextContentIcon-1LQXRB" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
+				<path fill-rule="evenodd" clip-rule="evenodd"
+					d="M6 2C3.79086 2 2 3.79086 2 6V18C2 20.2091 3.79086 22 6 22H18C20.2091 22 22 20.2091 22 18V6C22 3.79086 20.2091 2 18 2H6ZM10 8C10 6.8952 9.1032 6 8 6C6.8944 6 6 6.8952 6 8C6 9.1056 6.8944 10 8 10C9.1032 10 10 9.1056 10 8ZM9 14L6 18H18L15 11L11 16L9 14Z"
+				fill="currentColor">
+				</path>
+			</svg>
+			`:""}
 		</div>
 		`
 		el.innerText = "";
